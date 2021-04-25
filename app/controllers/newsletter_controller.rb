cfa5b88e54b2062dee9fd0f11540dd21b66278e6
RodrigoM-Git <rodrigomiguelcrojas@gmail.com>
2021-04-25 10:02:29 +0000

    Made email validation in controller

--- app/controllers/newsletter_controller.rb
+++ app/controllers/newsletter_controller.rb
@@ -2,7 +2,10 @@ class NewsletterController < ApplicationController
 
   def newsletter
     @email = params[:user_email]
-    NewsletterMailer.new_subscriber_email(@email).deliver
+
+    if @email != nil
+      NewsletterMailer.new_subscriber_email(@email).deliver
+    end
   end
 
 end
