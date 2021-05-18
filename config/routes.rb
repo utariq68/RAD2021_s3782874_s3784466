67f64f8dc976e02fcbbaa34990a937c387571d23
RodrigoM-Git <rodrigomiguelcrojas@gmail.com>
2021-05-18 04:18:44 +0000

    push

--- config/routes.rb
+++ config/routes.rb
@@ -38,5 +38,7 @@ Rails.application.routes.draw do
 
   post '/add_bag', to: 'items#add_bag'
 
+  get '/add_bag', to: 'items#add_bag'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
