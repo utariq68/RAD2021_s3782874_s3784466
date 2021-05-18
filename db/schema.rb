9ab63643009dd855e2198b993bcc89d22cddfdba
utariq68 <utariq68@users.noreply.github.com>
2021-05-18 18:13:49 +1000

    check this

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_17_152059) do
+ActiveRecord::Schema.define(version: 2021_05_18_070937) do
 
   create_table "bags", force: :cascade do |t|
     t.string "title"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2021_05_17_152059) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
